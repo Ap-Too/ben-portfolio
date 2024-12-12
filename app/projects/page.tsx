205eb7ec547acef5b4989fa63bf3c0dcb03cf001
Benjamin Allen <ben.allen.02.11@gmail.com>
2024-12-12 13:30:12 -0500

    Update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,6 +14,7 @@ import { projects } from '@/constants';
 
 
 const ProjectPage = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [currentIndex, setCurrentIndex]=useState(0)
   const handleSlideChange=(index:number)=>{
     setCurrentIndex(index)
